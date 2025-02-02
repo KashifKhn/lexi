e4fedbb5e6be9c7c8d525aba02f15e95713b9e29
KashifKhn <kashifkhnx04@gmail.com>
2025-02-02 18:19:50 +0500

    add code to main

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from chat_lexi import ChatLexi
 from logger import get_logger
+from text_to_speech import text_to_speech
+from wake_words_detector import WakeWordsDetector
 
 logger = get_logger()
 
-logger.info("Application start")
+
+wake_words = ["hey lexi", "hello lexi", "lexi"]
+
+wake_detector = WakeWordsDetector(wake_words)
+lexi = ChatLexi()
+
+if __name__ == "__main__":
+
+    while True:
+        if wake_detector.listen_for_wake_word():
+            logger.info("Wake word detected")
+            res = lexi.chat("Lexi initiate")
+            text_to_speech(res["response"])
+
